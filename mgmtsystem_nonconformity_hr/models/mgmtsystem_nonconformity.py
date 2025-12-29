cc73850456f810a73d3f0c439ab1348dc2980cae
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-12-29 17:17:43 -0300

    [MIG] mgmtsystem_nonconformity_hr

--- mgmtsystem_nonconformity_hr/models/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity_hr/models/mgmtsystem_nonconformity.py
@@ -5,13 +5,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 #
 ###############################################################################
-# Copyright 2019 Marcelo Frare (Ass. PNLUG - Gruppo Odoo <http://odoo.pnlug.it>)
-# Copyright 2019 Stefano Consolaro (Ass. PNLUG - Gruppo Odoo <http://odoo.pnlug.it>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
-class MgmtsystemMgmHR(models.Model):
-    _inherit = ["mgmtsystem.nonconformity"]
+class MgmtsystemNonconformity(models.Model):
+    _inherit = "mgmtsystem.nonconformity"
 
-    department_id = fields.Many2one("hr.department", "Department")
+    department_id = fields.Many2one("hr.department", string="Department")
